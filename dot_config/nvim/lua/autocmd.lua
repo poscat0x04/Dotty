e6293999d59762bc81fcd77f831a1ba217fcf15d
poscat <poscat@poscat.moe>
2024-05-15 16:50:03 +0800

    nvim: fix barbecue autocmd

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -57,6 +57,4 @@ vim.api.nvim_create_autocmd('User',
 
 -- barbecue #92
 vim.api.nvim_create_autocmd('TermEnter',
-{ callback = function() require('barbecue.ui').update() end
-, group = vim.api.nvim_create_augroup('barbecue.updater', {})
-})
+{ callback = function() require('barbecue.ui').update() end })
